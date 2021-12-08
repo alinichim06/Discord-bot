677788b0a9018d15e62ff46d57fd164a53ba21cf
Alin Ichim <alin.ichim@stud.acs.upb.ro>
2021-12-08 18:24:51 +0200

    kjdsbf
    Signed-off-by: Alin Ichim <alin.ichim@stud.acs.upb.ro>

--- bot-skel.py
+++ bot-skel.py
@@ -1,4 +1,4 @@
-
+# Copyright GPL-3.0-only 2021 Alin Ichim
     
 
     #!./.venv/bin/python
@@ -196,3 +196,15 @@ if __name__ == '__main__':
     bot.run(os.environ['BOT_TOKEN'])
     
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
